2f60e2506b5d94d04f3c077a73076ed0959616a8
not-saved <corradin.loris@gmail.com>
2023-05-29 11:15:22 +0200

    added getTenantInfo method

--- test/test.js
+++ test/test.js
@@ -2,3 +2,4 @@ import { TranslateLib } from "translate-lib";
 
 const lib = new TranslateLib({ token: "a9477290-9047-5d96-b11c-1f82994f0aa2" });
 lib.getTranslations("en").then(console.log);
+lib.getTenantInfo().then(console.log);
